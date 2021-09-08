c6af422904477ce027617861c230bddf22f3724a
LK <i91935058@163.com>
2021-09-08 16:24:21 +0800

    test: add test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,11 +1,39 @@
+import { orientation } from 'react-native-sensors'
 import { renderHook } from '@testing-library/react-hooks'
 import useDeviceOrientationValue from '../src'
 
-it('should return desired value', () => {
-  const hook = renderHook(() => useDeviceOrientationValue())
+const unsubscribe = jest.fn()
+jest.mock('react-native-sensors', () => ({
+  __esModule: true,
+  orientation: {
+    subscribe: jest.fn((cb) => {
+      cb({ qw: 0, qx: 0, qy: 0, qz: 0 })
+      return {
+        unsubscribe,
+      }
+    }),
+  },
+}))
 
-  expect(hook.result.current.rotation).toHaveProperty('x')
-  expect(hook.result.current.rotation).toHaveProperty('y')
+it('should subscribe and unsubscribe orientation event', () => {
+  const { unmount } = renderHook(() => useDeviceOrientationValue())
+  expect(orientation.subscribe).toHaveBeenCalledTimes(1)
+
+  unmount()
+
+  expect(unsubscribe).toHaveBeenCalledTimes(1)
+})
+
+it('should return desired value', () => {
+  const { result } = renderHook(() => useDeviceOrientationValue())
+
+  expect(result.current.rotation).toBeInstanceOf(Object)
+
+  expect(result.current.rotation).toHaveProperty('x')
+  expect(result.current.rotation).toHaveProperty('y')
+
+  expect(typeof result.current.subscribe).toBe('function')
+  expect(typeof result.current.unsubscribe).toBe('function')
 })
 
 it('should throw error when props is not valid type', () => {})
